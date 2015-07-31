6bcfca22edebdc676259e8bb1bd60a1e7aa8ea15
xit4 <v.labalestra@gmail.com>
2015-07-31 15:43:36 +0200

    Initial commit (v14)

---
+++ src/bot/BotStarter.java
@@ -0,0 +1,195 @@
+// Copyright 2015 theaigames.com (developers@theaigames.com)
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//	
+//    For the full copyright and license information, please view the LICENSE
+//    file that was distributed with this source code.
+
+package bot;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import field.*;
+import moves.MoveType;
+
+/**
+ * BotStarter class
+ * 
+ * This class is where the main logic should be. Implement getMoves() to
+ * return something better than random moves.
+ * 
+ * @author Jim van Eeden <jim@starapple.nl>
+ */
+
+public class BotStarter {
+
+	public BotStarter() {}
+
+/*	private final double heightWeight = 0.510066;
+	private final double linesWeight =0.760666;
+	private final double holesWeight =0.35663;
+	private final double bumpinessWeight =0.184483;*/
+	/**
+	 * Returns a random amount of random moves
+	 * @param state : current state of the bot
+	 * @param timeout : time to respond
+	 * @return : a list of moves to execute
+	 */
+	public ArrayList<MoveType> getMoves(BotState state, long timeout) {
+
+		ArrayList<MoveType> bestMoves;
+		if(state.getMyField().tooHigh())
+			bestMoves = getGreedyMoves(state,timeout);
+		else
+			bestMoves = getBuildingMoves(state,timeout);
+		bestMoves.add(MoveType.DROP);
+		return bestMoves;
+
+	}
+
+	public ArrayList<MoveType> getGreedyMoves(BotState state, long timeout) {
+
+		final double heightWeight = 0.510066;
+		final double linesWeight =0.760666;
+		final double holesWeight =0.35663;
+		final double bumpinessWeight =0.184483;
+
+		ArrayList<MoveType> bestMoves = new ArrayList<MoveType>();
+		int bestLeft=0,bestRotation=0;
+		double bestScore = 0.0;
+		int left=0;
+		Field grid = state.getMyField();
+		ShapeType workingPiece = state.getCurrentShape();
+		Shape piece = new Shape(workingPiece,grid,state.getShapeLocation());
+		if(piece.getLocation().getY()<0)
+			piece.oneDown();
+		for(int rotation = 0; rotation < 4; rotation++){
+			left=0;
+			if(rotation !=0) {
+				piece.turnRight();
+			}
+			Shape _piece = piece.clone();
+			while(grid.canMoveLeft(_piece)){
+				_piece.oneLeft();
+				left++;
+			}
+
+			while(grid.isValid(_piece)){
+				Shape _setPiece = _piece.clone();
+
+				while(grid.canMoveDown(_setPiece)){
+					_setPiece.oneDown();
+				}
+
+				double score;
+
+				Field _grid = grid.clone();
+				_grid.addPiece(_setPiece);
+
+				score = -heightWeight * _grid.aggregateHeight() + linesWeight * _grid.lines() - holesWeight * _grid.holes() - bumpinessWeight * _grid.bumpiness();
+
+				if (score > bestScore || bestScore == 0.0){
+					bestScore = score;
+					bestLeft = left;
+					bestRotation = rotation;
+				}
+
+				left--;
+				_piece.oneRight();
+			}
+		}
+		for(;bestRotation>0;bestRotation--)
+			bestMoves.add(MoveType.TURNRIGHT);
+		if(bestLeft<0)
+			for(;bestLeft<0;bestLeft++)
+				bestMoves.add(MoveType.RIGHT);
+		else
+			for(;bestLeft>0;bestLeft--)
+				bestMoves.add(MoveType.LEFT);
+		return bestMoves;
+
+	}
+
+	public ArrayList<MoveType> getBuildingMoves(BotState state, long timeout) {
+
+		final double heightWeight = 0.510066;
+		final double linesWeight =0.35663;
+		final double holesWeight =0.760666;
+		final double bumpinessWeight =0.084483;
+
+		ArrayList<MoveType> bestMoves = new ArrayList<MoveType>();
+		int bestLeft=0,bestRotation=0;
+		double bestScore = 0.0;
+		int left=0;
+		Field grid = state.getMyField();
+		ShapeType workingPiece = state.getCurrentShape();
+		Shape piece = new Shape(workingPiece,grid,state.getShapeLocation());
+		if(piece.getLocation().getY()<0)
+			piece.oneDown();
+		for(int rotation = 0; rotation < 4; rotation++){
+			left=0;
+			if(rotation !=0) {
+				piece.turnRight();
+			}
+			Shape _piece = piece.clone();
+			while(grid.canMoveLeft(_piece)){
+				_piece.oneLeft();
+				left++;
+			}
+
+			while(grid.isValidTweak(_piece)){
+				Shape _setPiece = _piece.clone();
+
+				while(grid.canMoveDown(_setPiece)){
+					_setPiece.oneDown();
+				}
+
+				double score;
+
+				Field _grid = grid.clone();
+				_grid.addPiece(_setPiece);
+
+				score = -heightWeight * _grid.aggregateHeight() + linesWeight * _grid.lines() - holesWeight * _grid.holes() - bumpinessWeight * _grid.bumpiness();
+
+				if (score > bestScore || bestScore == 0.0){
+					bestScore = score;
+					bestLeft = left;
+					bestRotation = rotation;
+				}
+
+				left--;
+				_piece.oneRight();
+			}
+		}
+		for(;bestRotation>0;bestRotation--)
+			bestMoves.add(MoveType.TURNRIGHT);
+		if(bestLeft<0)
+			for(;bestLeft<0;bestLeft++)
+				bestMoves.add(MoveType.RIGHT);
+		else
+			for(;bestLeft>0;bestLeft--)
+				bestMoves.add(MoveType.LEFT);
+		return bestMoves;
+
+	}
+	
+	public static void main(String[] args)
+	{
+		BotParser parser = new BotParser(new BotStarter());
+		parser.run();
+	}
+}
---
+++ src/field/Field.java
@@ -0,0 +1,202 @@
+// Copyright 2015 theaigames.com (developers@theaigames.com)
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//	
+//    For the full copyright and license information, please view the LICENSE
+//    file that was distributed with this source code.
+
+package field;
+
+import field.Cell;
+
+/**
+ * Field class
+ * 
+ * Represents the playing field for one player.
+ * Has some basic methods already implemented.
+ * 
+ * @author Jim van Eeden <jim@starapple.nl>
+ */
+
+public class Field {
+	
+	private int width;
+	private int height;
+	private String initialField;
+	private Cell grid[][];
+
+	public Field(int width, int height, String fieldString) {
+		this.width = width;
+		this.height = height;
+		this.initialField = fieldString;
+		parse(fieldString);
+	}
+	
+	/**
+	 * Parses the input string to get a grid with Cell objects
+	 * @param fieldString : input string
+	 */
+	private void parse(String fieldString) {
+		
+		this.grid = new Cell[this.width][this.height];
+		
+		// get the separate rows
+		String[] rows = fieldString.split(";");
+		for(int y=0; y < this.height; y++) {
+			String[] rowCells = rows[y].split(",");
+			
+			// parse each cell of the row
+			for(int x=0; x < this.width; x++) {
+				int cellCode = Integer.parseInt(rowCells[x]);
+				this.grid[x][y] = new Cell(x, y, CellType.values()[cellCode]);
+			}
+		}
+	}
+	
+	public Cell getCell(int x, int y) {
+		if(x < 0 || x >= this.width || y < 0 || y >= this.height)
+			return null;
+		return this.grid[x][y];
+	}
+
+	private void setCell(Cell cell) {
+		int x = (int) cell.getLocation().getX();
+		int y = (int) cell.getLocation().getY();
+		if(x < 0 || x >= this.width || y < 0 || y >= this.height)
+			return;
+		this.grid[x][y].setBlock();
+	}
+
+	public void addPiece(Shape piece){
+		for(Cell single : piece.getBlocks()){
+			setCell(single);
+		}
+	}
+
+	private int columnHeight(int column){
+		int r = 0;
+		for(; r < this.height && (this.grid[column][r].isEmpty()||this.grid[column][r].isShape()); r++);
+		return this.height - r-1;
+	}
+
+	public int aggregateHeight(){
+		int total = 0;
+		for(int c = 0; c < this.width; c++){
+			total += this.columnHeight(c);
+		}
+		return total;
+	}
+
+	private boolean isLine(int row){
+		for(int c = 0; c < this.width; c++){
+			if (this.grid[c][row].isEmpty() || this.grid[c][row].isSolid()){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public int holes(){
+		int count = 0;
+		for(int c = 0; c < this.width; c++){
+			boolean block = false;
+			for(int r = 0; r < this.height; r++){
+				if (this.grid[c][r].isBlock()) {
+					block = true;
+				}else if (this.grid[c][r].isEmpty() && block){
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	public int bumpiness(){
+		int total = 0;
+		for(int c = 0; c < this.width - 1; c++){
+			total += Math.abs(this.columnHeight(c) - this.columnHeight(c+ 1));
+		}
+		return total;
+	}
+
+	public int lines(){
+		int count = 0;
+		for(int r = 0; r < this.height; r++){
+			if (this.isLine(r)){
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public boolean canMoveLeft(Shape piece){
+		Shape tempPiece = piece.clone();
+		tempPiece.oneLeft();
+		Cell[] tempBlocks = tempPiece.getBlocks();
+		for(Cell single : tempBlocks){
+			if(single.hasCollision(this) || single.isOutOfBoundaries(this))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean isValid(Shape piece){
+		Cell[] tempBlocks = piece.getBlocks();
+		for(Cell single : tempBlocks){
+			if(single.hasCollision(this) || single.isOutOfBoundaries(this))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean isValidTweak(Shape piece){
+		Cell[] tempBlocks = piece.getBlocks();
+		for(Cell single : tempBlocks){
+			if(single.hasCollision(this) || single.isOutOfBoundaries(this) || single.getLocation().getX() == this.getWidth()-1)
+				return false;
+		}
+		return true;
+	}
+
+	public boolean tooHigh(){
+		for(int c = 0; c < this.width; c++){
+			if (this.columnHeight(c)>=this.getHeight()/2)
+				return true;
+		}
+		return false;
+	}
+
+	public boolean canMoveDown(Shape piece){
+		Shape tempPiece = piece.clone();
+		tempPiece.oneDown();
+		Cell[] tempBlocks = tempPiece.getBlocks();
+		for(Cell single : tempBlocks){
+			if(single.hasCollision(this) || single.isOutOfBoundaries(this))
+				return false;
+		}
+		return true;
+	}
+
+	public Field clone(){
+		return new Field(this.width,this.height,this.initialField);
+	}
+
+
+	public int getHeight() {
+		return this.height;
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
+}
